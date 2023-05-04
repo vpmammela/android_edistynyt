a1ebd957a0f439a1dcde5741c2fda2254382eefa
vmammela <veli-pekka.mammela@edu.lapinamk.fi>
2023-05-04 19:35:02 +0300

    lecture 10: customViewTesterFragment and CustomTemperatureView added

---
+++ app/src/main/java/com/example/edistynytandroid/CustomTemperatureView.kt
@@ -0,0 +1,73 @@
+package com.example.edistynytandroid
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class CustomTemperatureView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    // your helper variables etc. can be here
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+
+    init
+    {
+        // this is constructor of your component
+        // all initializations go here
+        paint.color = Color.BLUE
+        textPaint.color = Color.WHITE
+        textPaint.textSize = 70f
+        textPaint.textAlign = Paint.Align.CENTER
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // you can do all the drawing through the canvas-object
+        // parameters: x-coordinate, y-coordinate, size, color
+        canvas.drawCircle(width.toFloat() / 2, width.toFloat() / 2, width.toFloat() / 2, paint)
+
+        // parameters: content, x, y, color
+        canvas.drawText("Test!", width.toFloat() / 2, width.toFloat() / 2 + 25, textPaint);
+        // here you can do all the drawing
+    }
+
+
+    val size = 200
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // Try for a width based on our minimum
+        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        var w: Int = View.resolveSizeAndState(minw, widthMeasureSpec, 1)
+
+        // if no exact size given (either dp or match_parent)
+        // use this one instead as default (wrap_content)
+        if (w == 0)
+        {
+            w = size * 2
+        }
+
+        // Whatever the width ends up being, ask for a height that would let the view
+        // get as big as it can
+        // val minh: Int = View.MeasureSpec.getSize(w) + paddingBottom + paddingTop
+        // in this case, we use the height the same as our width, since it's a circle
+        val h: Int = View.resolveSizeAndState(
+            View.MeasureSpec.getSize(w),
+            heightMeasureSpec,
+            0
+        )
+
+        setMeasuredDimension(w, h)
+    }
+
+// note, if you use this version, in your onDraw-method, remember to use the
+// canvas size when drawing the circle. for example:
+// canvas.width.toFloat()
+}
--- app/src/main/java/com/example/edistynytandroid/MainActivity.kt
+++ app/src/main/java/com/example/edistynytandroid/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.dataFragment,
                 R.id.commentDataFragment, R.id.mapsFragment, R.id.toDoDataFragment, R.id.openStreetMapsFragment,
                 R.id.feedbackReadFragment, R.id.feedbackSendFragment, R.id.weatherStationFragment,
-                R.id.remoteMessageFragment, R.id.localMqttFragment
+                R.id.remoteMessageFragment, R.id.localMqttFragment, R.id.customViewTesterFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
